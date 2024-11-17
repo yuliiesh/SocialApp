ff126ef9bccbac71f8e27db6b1f5151ce61cec39
Yuliia Sharko <sharkou1995@gmail.com>
2024-11-17 16:40:23 +0200

    - fix

--- SocialApp.GraphData/Neo4jDbContext.cs
+++ SocialApp.GraphData/Neo4jDbContext.cs
@@ -7,6 +7,7 @@ public class Neo4jDbContext
     public Neo4jDbContext(string uri, string username, string password)
     {
         Client = new GraphClient(new Uri(uri), username, password);
+        Client.ConnectAsync().Wait();
     }
 
     public IGraphClient Client { get; }
